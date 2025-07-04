89656c204a8f53e9c1e7589e466c8c2a32fb7cee
Chris Manson <chris@manson.ie>
2025-07-04 10:27:15 +0100

    stop using ember-cli for global css

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'scoped-css-test/config/environment';
 import { importSync, isDevelopingApp, macroCondition } from '@embroider/macros';
+import './css/app.css';
 
 if (macroCondition(isDevelopingApp())) {
   importSync('./deprecation-workflow');
